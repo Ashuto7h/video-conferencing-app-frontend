594a17b28669632de74ea19c29aedded94f4c246
Ashuto7h <ashusahu198@gmail.com>
2022-04-22 14:57:44 +0530

    heroku deploy fix

--- src/config/production.js
+++ src/config/production.js
@@ -1,6 +1,7 @@
 export default {
-  apiBaseUrl: 'https://video-con4ence.herokuapp.com/api/v1',
-  appUrl: 'https://video-con4enc.herokuapp.com',
-  peerHost: 'video-con4ence.herokuapp.com',
-  webSocketUrl: 'ws://video-con4ence-be.herokuapp.com'
+  apiBaseUrl: 'https://video-con4ence-be.herokuapp.com/api/v1',
+  appUrl: 'https://video-con4ence.herokuapp.com',
+  peerHost: 'video-con4ence-be.herokuapp.com',
+  peerSecure: true,
+  webSocketUrl: 'wss://video-con4ence-be.herokuapp.com'
 };
